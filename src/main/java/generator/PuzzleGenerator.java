36f13eea616459e2731f73517a3d7cd158dde714
frogy194 <frogy194@interia.pl>
2018-09-19 23:58:32 +0200

    no idea what's next

--- src/main/java/generator/PuzzleGenerator.java
+++ src/main/java/generator/PuzzleGenerator.java
@@ -2,8 +2,22 @@ package generator;
 
 public class PuzzleGenerator {
 
-//    public boolean checkIfSolvable (int[] sudoku) {
-//
-//
-//    }
+    public static int[] checkIfSolvable (int[] sudoku) {
+        int[] result = sudoku;
+        if (sudoku.length<81) {
+            System.out.println("Size of sudoku doesn't apply");
+            return result;
+        }
+
+        int[][] column = new int[8][8];
+        int[][] row = new int[8][8];
+        int[][] block = new int[8][8];
+
+        for (int i = 0; i < 81; i++) {
+
+
+        }
+    }
 }
+
+// petla dla kazdej kolumny i * 9 + j, i % 9 = numer kolumny , wiersza j % 9, i = numer wiersza, bloku
